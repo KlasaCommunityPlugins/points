90abcd40c418394bf21657a2b414e6e7019d62ce
QuantumlyTangled <EthoArx@gmail.com>
2019-03-22 16:17:28 +0100

    Beginning

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// Copyright (c) 2018-2019 KlasaCommunityPlugins. All rights reserved. MIT license.
+/**
+ * @external KlasaClient
+ * @see {@link https://klasa.js.org/#/docs/klasa/master/class/KlasaClient}
+ */
---
+++ src/lib/Client.ts
@@ -0,0 +1,11 @@
+// Copyright (c) 2018-2019 KlasaCommunityPlugins. All rights reserved. MIT license.
+import { Client } from 'klasa';
+
+/**
+ * The client for handling everything. See {@tutorial GettingStarted} for more information how to get started using this class.
+ * @extends external:KlasaClient
+ * @tutorial GettingStarted
+ */
+export class PointsClient extends Client {
+
+}
