0d414f0c1345cf1b9c766788e8ecc405681b8d0c
Antonio Román <kyradiscord@gmail.com>
2019-07-11 09:28:26 +0200

    fix: PointsUsers being incorrectly extended and docs

--- src/lib/extensions/PointsUser.ts
+++ src/lib/extensions/PointsUser.ts
@@ -1,18 +1,14 @@
 // Copyright (c) 2018-2019 KlasaCommunityPlugins. All rights reserved. MIT license.
-import { Structures, User } from 'discord.js';
+import { Structures } from 'discord.js';
 import { Client as KClient } from 'klasa';
 import Points from '../util/Points';
 
-class PointsUser extends User {
+Structures.extend('User', User => class PointsUser extends User {
 	/**
 	 * The handler for the users points
 	 * @since 0.0.1
-	 * @type {Points}
-	 * @protected
 	 */
-	points: Points = new Points(this, (this.client as KClient));
-}
-
-Structures.extend('User', () => PointsUser);
+	public points: Points = new Points(this, (this.client as KClient));
+});
 
 export { PointsUser };
